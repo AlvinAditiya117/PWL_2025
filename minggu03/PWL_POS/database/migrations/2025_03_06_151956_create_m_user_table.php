5320e6f23b3c0cb5a9d8cbd75ab650b9dbc00d71
AlvinAditiya117 <ryuzavin@gmail.com>
2025-03-07 03:14:24 +0700

    3 data seeder no10

--- minggu03/PWL_POS/database/migrations/2025_03_06_151956_create_m_user_table.php
+++ minggu03/PWL_POS/database/migrations/2025_03_06_151956_create_m_user_table.php
@@ -9,19 +9,23 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up() {
+    public function up(): void
+    {
         Schema::create('m_user', function (Blueprint $table) {
             $table->id('user_id');
             $table->unsignedBigInteger('level_id');
-            $table->string('username', 20);
-            $table->string('password', 255);
+            $table->string('username', 50)->unique();
+            $table->string('password');
+            $table->string('nama', 100); 
             $table->timestamps();
-
-            $table->foreign('level_id')->references('level_id')->on('m_level')->onDelete('cascade');
         });
     }
 
-    public function down() {
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
         Schema::dropIfExists('m_user');
     }
 };
