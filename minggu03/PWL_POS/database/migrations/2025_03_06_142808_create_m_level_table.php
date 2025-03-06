ab7bb9865a26d6076a65702c53bd88aa3008c558
AlvinAditiya117 <ryuzavin@gmail.com>
2025-03-07 00:51:13 +0700

    3 buat userseeder dan perbaikan m user no5

--- minggu03/PWL_POS/database/migrations/2025_03_06_142808_create_m_level_table.php
+++ minggu03/PWL_POS/database/migrations/2025_03_06_142808_create_m_level_table.php
@@ -9,16 +9,21 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up() {
+    public function up(): void
+    {
         Schema::create('m_level', function (Blueprint $table) {
             $table->id('level_id');
-            $table->string('kode', 10);
-            $table->string('nama_level', 100);
+            $table->string('level_kode', 10)->unique();
+            $table->string('level_nama', 100);
             $table->timestamps();
         });
     }
 
-    public function down() {
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
         Schema::dropIfExists('m_level');
     }
 };
