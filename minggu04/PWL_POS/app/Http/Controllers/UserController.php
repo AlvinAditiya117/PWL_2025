d3d71db7df6ed994f8bf0ccd2a22ebcca03b3d92
AlvinAditiya117 <ryuzavin@gmail.com>
2025-03-13 03:50:09 +0700

    Prak 2.7 No 02 ubah userController

--- minggu04/PWL_POS/app/Http/Controllers/UserController.php
+++ minggu04/PWL_POS/app/Http/Controllers/UserController.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index()
-    {
+    // public function index()
+    // {
         // $user = UserModel::all(); 
         // return view('user', ['data' => $user]);
 
@@ -180,50 +180,59 @@ class UserController extends Controller
 
         // jobsheet 4 praktikum 2.6
 
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
-    }
+    //     $user = UserModel::all();
+    //     return view('user', ['data' => $user]);
+    // }
 
-    public function tambah()
-    {
-        return view('user_tambah');
-    }
+    // public function tambah()
+    // {
+    //     return view('user_tambah');
+    // }
 
-    public function tambah_simpan(Request $request)
-    {
-        UserModel::create([
-            'username' => $request->username,
-            'nama' => $request->nama,
-            'password' => Hash::make($request->password),
-            'level_id' => $request->level_id,
-        ]);
+    // public function tambah_simpan(Request $request)
+    // {
+    //     UserModel::create([
+    //         'username' => $request->username,
+    //         'nama' => $request->nama,
+    //         'password' => Hash::make($request->password),
+    //         'level_id' => $request->level_id,
+    //     ]);
 
-        return redirect('/user');
-    }
-    public function ubah($id)
-    {
-        $user = UserModel::find($id);
-        return view('user_ubah', ['data' => $user]);
-    }
-    public function ubah_simpan($id, Request $request)
-    {
-        $user = UserModel::find($id);
+    //     return redirect('/user');
+    // }
+    // public function ubah($id)
+    // {
+    //     $user = UserModel::find($id);
+    //     return view('user_ubah', ['data' => $user]);
+    // }
+    // public function ubah_simpan($id, Request $request)
+    // {
+    //     $user = UserModel::find($id);
 
-        $user->username = $request->username;
-        $user->nama = $request->nama;
-        $user->password = Hash::make($request->password);
-        $user->level_id = $request->level_id;
+    //     $user->username = $request->username;
+    //     $user->nama = $request->nama;
+    //     $user->password = Hash::make($request->password);
+    //     $user->level_id = $request->level_id;
 
-        $user->save();
+    //     $user->save();
 
-        return redirect('/user');
-    }
+    //     return redirect('/user');
+    // }
     
-    public function hapus($id)
-    {
-        $user = UserModel::find($id);
-        $user->delete();
+    // public function hapus($id)
+    // {
+    //     $user = UserModel::find($id);
+    //     $user->delete();
 
-        return redirect('/user');
-    }
+    //     return redirect('/user');
+    // }
+
+    // Jobsheet 4 praktikum 2.7
+
+    
+    public function index()
+{
+    $user = UserModel::with('level')->get();
+    dd($user);
+}
 }
