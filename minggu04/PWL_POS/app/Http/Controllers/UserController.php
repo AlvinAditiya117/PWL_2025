e4bdd8bf90735e7adaa781b7ebfbac14207dc152
AlvinAditiya117 <ryuzavin@gmail.com>
2025-03-12 17:32:44 +0700

    Prak 2.4 No 06 ubah userController

--- minggu04/PWL_POS/app/Http/Controllers/UserController.php
+++ minggu04/PWL_POS/app/Http/Controllers/UserController.php
@@ -98,13 +98,22 @@ class UserController extends Controller
 
         //Jobsheet 4 Praktikum 2.4
 
-        $user = UserModel::firstOrCreate(
+        // $user = UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager22',
+        //         'nama' => 'Manager Dua Dua',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ],
+        // );
+
+        // return view('user', ['data' => $user]);
+        
+        $user = UserModel::firstOrNew(
             [
-                'username' => 'manager22',
-                'nama' => 'Manager Dua Dua',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
+                'username' => 'manager',
+                'nama' => 'Manager',
+            ]
         );
 
         return view('user', ['data' => $user]);
