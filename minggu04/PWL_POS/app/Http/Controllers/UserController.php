a8ba3cbd98df4b51b1289d2466cfe02f10f87970
AlvinAditiya117 <ryuzavin@gmail.com>
2025-03-13 00:01:28 +0700

    Prak 2.5 No 01 ubah UserController

--- minggu04/PWL_POS/app/Http/Controllers/UserController.php
+++ minggu04/PWL_POS/app/Http/Controllers/UserController.php
@@ -118,16 +118,45 @@ class UserController extends Controller
 
         // return view('user', ['data' => $user]);
 
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager33',
+        //         'nama' => 'Manager Tiga Tiga',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ],
+        // );
+        // $user->save();
+
+        // return view('user', ['data' => $user]);   
+        
+
+        // Jobsheet 4 praktikum 2.5
+
+        $user = UserModel::create([
+            'username' => 'manager55',
+            'password' => Hash::make('12345'),
+            'nama' => 'Manager55',
+            'level_id' => 2,
+        ]);
+
+        $user->username = 'manager56';
+
+        $user->isDirty(); // true
+        $user->isDirty('username'); // true
+        $user->isDirty('nama'); // false
+        $user->isDirty(['nama', 'username']); // true
+
+        $user->isClean(); // false
+        $user->isClean('username'); // false
+        $user->isClean('nama'); // true
+        $user->isClean(['nama', 'username']); // false
+
         $user->save();
 
-        return view('user', ['data' => $user]);     
+        $user->isDirty(); // false
+        $user->isClean(); // true 
+
+        dd($user->isDirty());
     }
 }
