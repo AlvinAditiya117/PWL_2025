51db55677c63bcd5ee1c04a480e270327679ce46
AlvinAditiya117 <ryuzavin@gmail.com>
2025-03-20 21:30:34 +0700

    Tugas Prak No 1 tambah code pada index dan perbaikan layouts

--- minggu05/PWL_POS/resources/views/Kategori/Index.blade.php
+++ minggu05/PWL_POS/resources/views/Kategori/Index.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.app')
+@extends('layouts.app')
 
 {{-- Customize layout sections --}}
 
@@ -6,12 +6,19 @@
 
 @section('content_header_title', 'Home')
 
-@section('content_header_subtitle', 'Kaggeori')
+@section('content_header_subtitle', 'Kategori')
 
 @section('content')
     <div class="container">
         <div class="card">
-            <div class="card-header">Manage Kategori</div>
+
+            <div class="card-header d-flex align-items-center">
+                <h3 class="card-title">Manage Kategori</h3>
+                <a href="{{ url('/kategori/create') }}" class="btn btn-primary ml-auto">
+                    Add New
+                </a>
+            </div>
+           
             <div class="card-body">
                 {{ $dataTable->table() }}
             </div>
