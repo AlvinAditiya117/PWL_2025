30cc49e7ed5ed2060f9c062cc8c7ada044138e92
AlvinAditiya117 <ryuzavin@gmail.com>
2025-03-27 00:47:10 +0700

    prak 2 no 4 edit /views/layouts/sidebar.blade.php

--- minggu05_new/PWL_POS/resources/views/layouts/sidebar.blade.php
+++ minggu05_new/PWL_POS/resources/views/layouts/sidebar.blade.php
@@ -1,4 +1,32 @@
-<!-- Sidebar -->
+<div class="sidebar">
+
+    <div class="form-inline mt-2">
+        <div class="input-group" data-widget="sidebar-search">
+            <input class="form-control form-control-sidebar" type="search"
+                   placeholder="Search" aria-label="Search">
+            <div class="input-group-append">
+                <button class="btn btn-sidebar">
+                    <i class="fas fa-search fa-fw"></i>
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview"
+            role="menu" data-accordion="false">
+            <li class="nav-item">
+                <a href="{{ url('/') }}" class="nav-link {{ ($activeMenu == 'dashboard') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>Dashboard</p>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</div>
+
+
+{{-- <!-- Sidebar -->
 <div class="sidebar">
     <!-- Sidebar user (optional) -->
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
@@ -661,4 +689,4 @@
       </ul>
     </nav>
     <!-- /.sidebar-menu -->
-  </div>
+  </div> --}}
