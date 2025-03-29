51c42d86e2cf52d2918b50a8fc226d2e0dc062c9
AlvinAditiya117 <ryuzavin@gmail.com>
2025-03-29 10:24:06 +0700

    prak 3 no 19 buat view edit data user di PWL/resources/views/user/edit.blade.php dan perbaikan userController

--- minggu05_new/PWL_POS/app/Http/Controllers/UserController.php
+++ minggu05_new/PWL_POS/app/Http/Controllers/UserController.php
@@ -135,7 +135,7 @@ class UserController extends Controller
         $request->validate([
             // username harus diisi, berupa string, minimal 3 karakter,
             // dan bernilai unik di tabel users kolom username kecuali untuk user dengan id yang sedang diedit
-            'username' => 'required|string|min:3|unique:users,username,' . $id,
+            'username' => 'required|string|min:3|unique:m_user,username,' . $id. ',user_id',
             'nama' => 'required|string|max:100', // nama harus diisi, berupa string, dan maksimal 100 karakter
             'password' => 'nullable|min:5', // password bisa diisi (minimal 5 karakter) atau tidak diisi
             'level_id' => 'required|integer' // level_id harus diisi dan berupa angka
