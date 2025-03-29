1f7b4f8bc54fb41e9bb37e70003b88b24b583694
AlvinAditiya117 <ryuzavin@gmail.com>
2025-03-29 21:30:06 +0700

    Tugas tabel m_kategori

--- minggu05_new/PWL_POS/resources/views/layouts/sidebar.blade.php
+++ minggu05_new/PWL_POS/resources/views/layouts/sidebar.blade.php
@@ -40,6 +40,7 @@
                     <p>Kategori Barang</p>
                 </a>
             </li>
+            
             <li class="nav-item">
                 <a href="{{ url('/barang') }}" class="nav-link {{ ($activeMenu == 'barang') ? 'active' : '' }}">
                     <i class="nav-icon far fa-list-alt"></i>
