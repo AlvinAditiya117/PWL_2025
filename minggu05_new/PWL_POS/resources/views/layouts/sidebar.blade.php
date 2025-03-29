c4ac05bfd63a9a663918faaeaf9afccb2c483ee8
AlvinAditiya117 <ryuzavin@gmail.com>
2025-03-29 22:01:20 +0700

    Tugas tabel m_supplier

--- minggu05_new/PWL_POS/resources/views/layouts/sidebar.blade.php
+++ minggu05_new/PWL_POS/resources/views/layouts/sidebar.blade.php
@@ -61,6 +61,14 @@
                     <p>Transaksi Penjualan</p>
                 </a>
             </li>
+
+            <div class="nav-header">Data Supplier</div>
+            <li class="nav-item">
+                <a href="{{ url('/supplier') }}" class="nav-link {{ ($activeMenu == 'supplier') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-cubes"></i>
+                    <p>Supplier</p>
+                </a>
+            </li>
         </ul>
     </nav>
 </div>
--- minggu05_new/PWL_POS/routes/web.php
+++ minggu05_new/PWL_POS/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SupplierController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -63,4 +64,15 @@ Route::group(['prefix' => 'kategori'], function () {
     Route::get('/{id}/edit', [KategoriController::class, 'edit']); // menampilkan halaman form edit user
     Route::put('/{id}', [KategoriController::class, 'update']); // menyimpan perubahan data user
     Route::delete('/{id}', [KategoriController::class, 'destroy']); // menghapus data user
+});
+
+Route::group(['prefix' => 'supplier'], function() {
+    Route::get('/', [SupplierController::class, 'index']);          //menampilkan halaman awal supplier
+    Route::post('/list', [SupplierController::class, 'list']);      //menampilkan data supplier dalam bentuk json untuk datatables
+    Route::get('/create', [SupplierController::class, 'create']);   //menampilkan halaman form tambah supplier
+    Route::post('/', [SupplierController::class, 'store']);         //menyimpan data supplier baru
+    Route::get('/{id}', [SupplierController::class, 'show']);       //menampilkan detail supplier
+    Route::get('/{id}/edit', [SupplierController::class, 'edit']);  //menamilkan halaman form edit supplier
+    Route::put('/{id}', [SupplierController::class, 'update']);     //menyimpan perubahan data supplier
+    Route::delete('/{id}', [SupplierController::class, 'destroy']); //menghapus data supplier
 });
