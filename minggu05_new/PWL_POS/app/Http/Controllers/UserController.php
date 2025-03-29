3130278e3af479c952011e7da0de7e4fd7313166
AlvinAditiya117 <ryuzavin@gmail.com>
2025-03-29 11:00:56 +0700

    prak 4 no 5  fungsi list() pada UserController.php

--- minggu05_new/PWL_POS/app/Http/Controllers/UserController.php
+++ minggu05_new/PWL_POS/app/Http/Controllers/UserController.php
@@ -55,6 +55,11 @@ class UserController extends Controller
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
 
+        // Filter data user berdasarkan level_id
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
+
         return DataTables::of($users)
             // menambahkan kolom Index no urut (default nama kolom: DT_RowIndex)
             ->addIndexColumn()
