5827dde1ef470b0dd9f315832c655eba1781b2dc
AlvinAditiya117 <ryuzavin@gmail.com>
2025-04-12 06:14:21 +0700

    prak 1 no 2 ubah userModel

--- minggu07/PWL_POS/app/Models/UserModel.php
+++ minggu07/PWL_POS/app/Models/UserModel.php
@@ -5,21 +5,51 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Monolog\Level;
 
-class UserModel extends Model
-{
-    use HasFactory;
-    protected $table = 'm_user';
-    protected $primaryKey = 'user_id'; 
+// class UserModel extends Model
+// {
+    // use HasFactory;
+    // protected $table = 'm_user';
+    // protected $primaryKey = 'user_id'; 
 
+    // // protected $fillable = ['level_id', 'username','nama','password'];
     // protected $fillable = ['level_id', 'username','nama','password'];
-    protected $fillable = ['level_id', 'username','nama','password'];
 
-    // Jobsheet 4 Praktikum 2.7
+    // // Jobsheet 4 Praktikum 2.7
 
+    // public function level(): BelongsTo
+    // {
+    //     return $this->belongsTo(LevelModel ::class, 'level_id', 'level_id');
+    // }
+
+    //jobsheet 7
+    class UserModel extends Authenticatable
+    {
+    use HasFactory;
+
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+    protected $fillable = [
+        'username', 'password', 'nama', 'level_id', 'created_at', 'updated_at',
+    ];
+
+    protected $hidden = [
+        'password', // jangan di tampilkan saat select
+    ];
+
+    protected $casts = [
+        'password' => 'hashed', // casting password agar otomatis di hash
+    ];
+
+    /**
+     * Relasi ke tabel level
+     *
+     * @return BelongsTo
+     */
     public function level(): BelongsTo
     {
-        return $this->belongsTo(LevelModel ::class, 'level_id', 'level_id');
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 }
