d2d7878e566d195d175daeab5d61ed317117c866
AlvinAditiya117 <ryuzavin@gmail.com>
2025-04-12 13:42:37 +0700

    prak 2 no 1 tambah userModel

--- minggu07/PWL_POS/app/Models/UserModel.php
+++ minggu07/PWL_POS/app/Models/UserModel.php
@@ -10,29 +10,34 @@ use Monolog\Level;
 
 // class UserModel extends Model
 // {
-    // use HasFactory;
-    // protected $table = 'm_user';
-    // protected $primaryKey = 'user_id'; 
+// use HasFactory;
+// protected $table = 'm_user';
+// protected $primaryKey = 'user_id'; 
 
-    // // protected $fillable = ['level_id', 'username','nama','password'];
-    // protected $fillable = ['level_id', 'username','nama','password'];
+// // protected $fillable = ['level_id', 'username','nama','password'];
+// protected $fillable = ['level_id', 'username','nama','password'];
 
-    // // Jobsheet 4 Praktikum 2.7
+// // Jobsheet 4 Praktikum 2.7
 
-    // public function level(): BelongsTo
-    // {
-    //     return $this->belongsTo(LevelModel ::class, 'level_id', 'level_id');
-    // }
+// public function level(): BelongsTo
+// {
+//     return $this->belongsTo(LevelModel ::class, 'level_id', 'level_id');
+// }
 
-    //jobsheet 7
-    class UserModel extends Authenticatable
-    {
+//jobsheet 7
+class UserModel extends Authenticatable
+{
     use HasFactory;
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
     protected $fillable = [
-        'username', 'password', 'nama', 'level_id', 'created_at', 'updated_at',
+        'username',
+        'password',
+        'nama',
+        'level_id',
+        'created_at',
+        'updated_at',
     ];
 
     protected $hidden = [
@@ -51,5 +56,20 @@ use Monolog\Level;
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+    /**
+     * Mendapatkan nama role
+     */
+    public function getRoleName(): string
+    {
+        return $this->level->level_name;
+    }
+
+    /**
+     * Cek apakah user memiliki rule tertentu
+     */
+    public function hasRole(bool $role): bool
+    {
+        return $this->level->level_kode == $role;
     }
 }
