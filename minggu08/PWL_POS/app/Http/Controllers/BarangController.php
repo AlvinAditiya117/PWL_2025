eacd00880c4b3e8dd32f722d4583c394b81ff87c
AlvinAditiya117 <ryuzavin@gmail.com>
2025-04-16 03:43:10 +0700

    prak 2 no 3 sampai 8 tambah export_excell pada barangController

--- minggu08/PWL_POS/app/Http/Controllers/BarangController.php
+++ minggu08/PWL_POS/app/Http/Controllers/BarangController.php
@@ -53,7 +53,7 @@ class BarangController extends Controller
                 $btn = '<button onclick="modalAction(\'' . url('/barang/' . $barang->barang_id
 
                     . '/show_ajax') . '\')" class="btn btn-info btn-sm">Detail</button> ';
-                    
+
                 $btn .= '<button onclick="modalAction(\'' . url('/barang/' . $barang->barang_id . '/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button> ';
                 $btn .= '<button onclick="modalAction(\'' . url('/barang/' . $barang->barang_id . '/delete_ajax') . '\')" class="btn btn-danger btn-sm">Hapus</button> ';
 
@@ -229,4 +229,66 @@ class BarangController extends Controller
         }
         return redirect('/');
     }
+
+    public function export_excel()
+    {
+        //Ambil value barang yang akan diexport
+        $barang = BarangModel::select(
+            'kategori_id',
+            'barang_kode',
+            'barang_nama',
+            'harga_beli',
+            'harga_jual'
+        )
+        ->orderBy('kategori_id')
+        ->with('kategori')
+        ->get();
+
+
+         //load library excel
+         $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+         $sheet = $spreadsheet->getActiveSheet(); //ambil sheet aktif
+ 
+         $sheet->setCellValue('A1', 'No');
+         $sheet->setCellValue('B1', 'Kode Barang');
+         $sheet->setCellValue('C1', 'Nama Barang');
+         $sheet->setCellValue('D1', 'Harga Beli');
+         $sheet->setCellValue('E1', 'Harga Jual');
+         $sheet->setCellValue('F1', 'Kategori');
+ 
+         $sheet->getStyle('A1:F1')->getFont()->setBold(true); // Set header bold
+ 
+         $no = 1; //Nomor value dimulai dari 1
+         $baris = 2; //Baris value dimulai dari 2
+         foreach ($barang as $key => $value) {
+             $sheet->setCellValue('A' . $baris, $no);
+             $sheet->setCellValue('B' . $baris, $value->barang_kode);
+             $sheet->setCellValue('C' . $baris, $value->barang_nama);
+             $sheet->setCellValue('D' . $baris, $value->harga_beli);
+             $sheet->setCellValue('E' . $baris, $value->harga_jual);
+             $sheet->setCellValue('F' . $baris, $value->kategori->kategori_nama); //ambil nama kategori
+             $no++;
+             $baris++;
+         }
+ 
+         foreach (range('A', 'F') as $columnID) {
+             $sheet->getColumnDimension($columnID)->setAutoSize(true); //set auto size untuk kolom
+         }
+ 
+         $sheet->setTitle('Data Barang'); //set judul sheet
+         $writer = IOFactory ::createWriter($spreadsheet, 'Xlsx'); //set writer
+         $filename = 'Data_Barang' . date('Y-m-d_H-i-s') . '.xlsx'; //set nama file
+ 
+         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+         header('Content-Disposition: attachment; filename="' . $filename . '"');
+         header('Cache-Control: max-age=0');
+         header('Cache-Control: max-age=1');
+         header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+         header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+         header('Cache-Control: cache, must-revalidate');
+         header('Pragma: public');
+ 
+         $writer->save('php://output'); 
+         exit; 
+     } // end function export_excel
 }
