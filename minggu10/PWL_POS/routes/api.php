096656df6ee61f15ab7b4e44bcd27733f63a24f3
AlvinAditiya117 <ryuzavin@gmail.com>
2025-05-08 22:25:52 +0700

    Prak 4 no 3 edit routes api

--- minggu10/PWL_POS/routes/api.php
+++ minggu10/PWL_POS/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController; 
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\LevelController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -26,4 +27,10 @@ Route::post('/login', LoginController::class)->name('login');
      return $request->user();
  });
 
- Route::post('/logout', LogoutController::class)->name('logout');
+Route::post('/logout', LogoutController::class)->name('logout');
+
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
